bf1d37878cf4d3041948e213bd5654451a969f05
Rocko <codingwithrock@gmail.com>
2022-09-21 19:05:36 +0600

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Countries from './Componenets/Countries/Countries';
+import Country from './Componenets/Country/Country';
 
 function App() {
   return (
     <div className="App">
       <h1>Country Details</h1>
       <Countries></Countries>
+      <Country></Country>
     </div>
   );
 }
--- src/Componenets/Country/Country.js
+++ src/Componenets/Country/Country.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Country.css';
 
 const Country = () => {
     return (
-        <div>
-            
+        <div className='bg-red-50'>
+            <img src="" alt="Flag"/>
+            <h2>Country Name</h2>
+            <h4>Country Population</h4>
+            <h4>Country Capital</h4>
         </div>
     );
 };
