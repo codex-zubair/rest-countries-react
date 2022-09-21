0c151650c5f41aa7c7368370c130fbf51095d52c
Rocko <codingwithrock@gmail.com>
2022-09-21 20:15:44 +0600

    fixed flax size

--- src/Components/Country/Country.js
+++ src/Components/Country/Country.js
@@ -7,7 +7,7 @@ const Country = (props) => {
     return (
 
         <div className='bg-red-50 rounded-md flex flex-col'>
-            <img src={flags.png} alt={name.common + "Flag"} />
+            <img className='w-10/12 mx-auto' src={flags.png} alt={name.common + "Flag"} />
             <h2 className='text-xl font-semibold'>{name.common}</h2>
             <h4 className='font-semibold'>Population: <span className='font-light'>{population}</span></h4>
             <h4 className='font-semibold'>Capital: <span>{capital}</span></h4>
