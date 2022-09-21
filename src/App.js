f453374954c682c8f6ab6d25d572a6fc65ff3295
Rocko <codingwithrock@gmail.com>
2022-09-21 18:56:41 +0600

    add connection between them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Countries from './Componenets/Countries/Countries';
 
 function App() {
   return (
     <div className="App">
       <h1>Country Details</h1>
+      <Countries></Countries>
     </div>
   );
 }
---
+++ src/Componenets/Countries/Countries.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            Hello Country!
+        </div>
+    );
+};
+
+export default Countries;
