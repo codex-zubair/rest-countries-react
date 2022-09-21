27c8aab86d49bb11de306a7b91768e1ad08e113d
Rocko <codingwithrock@gmail.com>
2022-09-21 20:12:24 +0600

    mobile and medium device responsive

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <h1  className='text-2xl font-semibold bg-slate-200 py-2 mb-5'>Country Details</h1>
 
       {/* Adding grid layout For each Country */}
-      <section className='grid grid-cols-3 gap-3 w-11/12 mx-auto mt-2'>
+      <section className='grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-3 w-11/12 mx-auto mt-2'>
       <Countries></Countries>
       </section>
       
