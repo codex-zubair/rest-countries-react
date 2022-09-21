d58613d279a5cded0fc5affc2ec3942be7c4897b
Rocko <codingwithrock@gmail.com>
2022-09-21 20:10:37 +0600

    added some css style and layout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Countries from './Componenets/Countries/Countries';
-import Country from './Componenets/Country/Country';
+import Countries from './Components/Countries/Countries';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-2xl font-semibold'>Country Details</h1>
+      <h1  className='text-2xl font-semibold bg-slate-200 py-2 mb-5'>Country Details</h1>
 
       {/* Adding grid layout For each Country */}
-      <section className='grid grid-cols-3 gap-2 w-11/12 mx-auto mt-2'>
+      <section className='grid grid-cols-3 gap-3 w-11/12 mx-auto mt-2'>
       <Countries></Countries>
       </section>
       
